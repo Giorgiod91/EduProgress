607d0c731df3a539256f0aec7c63154b8fafb461
Giorgiod91 <giorgio.dettmar@gmx.de>
2024-07-15 22:06:16 +0200

    chnaged ladingpage slogan

--- src/app/components/LandingPage.tsx
+++ src/app/components/LandingPage.tsx
@@ -13,20 +13,20 @@ function LandingPage({}: Props) {
         </h1>
         <div>
           <a
-            href=""
+            href="/signup"
             className="btn-gradient animate-shimmer btn btn-primary btn-wide"
           >
-            Lets go
+            Get Started
           </a>
         </div>
-      </div>
-      <div className="h-[600px] w-[600px] lg:w-full lg:items-end lg:justify-end">
-        <Image
-          src="/screen.png"
-          alt="screen"
-          layout="fill"
-          objectFit="contain"
-        />
+        <div className="flex flex-col items-center justify-center gap-10 text-center lg:items-start lg:gap-8 lg:text-left">
+          <h2 className="text-3xl font-bold leading-tight md:text-4xl lg:text-5xl">
+            Are you doing a degree or do you want to learn a new language ?
+            Track your progress and stay motivated
+          </h2>
+          <ul className="text-base-content-secondary max-w-lg leading-relaxed"></ul>
+        </div>
+        <div className="h-[600px] w-[600px] lg:w-full lg:items-end lg:justify-end"></div>
       </div>
     </div>
   );
