19539f36bee2e43a5cd25669858458cbc796bdd0
Giorgiod91 <giorgio.dettmar@gmx.de>
2024-07-21 21:55:22 +0200

    ss

--- src/app/api/stripe/route/route.ts
+++ src/app/api/stripe/route/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
-
+// most of this code is copied from the stripe documentation or example from youtube
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: "2024-06-20",
 });
